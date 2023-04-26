a4b7d2ef436c2892891d75fb0cfb60f456204557
Jay Mathis <code@mathis.network>
2023-04-26 18:46:51 -0500

    add missing declarations

--- src/mana.ts
+++ src/mana.ts
@@ -16,8 +16,8 @@ export type Options = {
 export interface IMANAComponents {
   balance: (from?: string) => Promise<string>
   allowance: (spenderAddress: string, from?: string) => Promise<string>
-  approve: (spenderAddress: string, amount?: eth.BigNumber) => Promise<string>
-  transfer: (to: string, amount: eth.BigNumber) => Promise<string>
+  approve: (spenderAddress: string, amount?: eth.BigNumber, options?: Options) => Promise<string>
+  transfer: (to: string, amount: eth.BigNumber, options?: Options) => Promise<string>
   //depositMana: (amount: number) => Promise<string>
 }
 
